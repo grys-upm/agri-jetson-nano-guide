09b42273c4870473dade7cb74875ccd031b74783
albertosanchez33 <alberto.sanchez33@alumnos.upm.es>
2025-06-05 13:12:35 +0200

    Delete run_unetRT.sh

--- run_unetRT.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Ejecutando modelo UNet con LD_PRELOAD..."
-LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 python3 /home/jetson/Desktop/CWFID/UNet_Trt.py
-echo "Script finalizado."
